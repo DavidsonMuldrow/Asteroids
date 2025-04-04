c31c06bbc46bc49f31ad55b03334f8ea2db72a41
DavidsonMuldrow <dm882824@ohio.edu>
2025-04-04 13:51:35 -0400

    asteroids2

--- Asteroids/Assets/Player.cs
+++ Asteroids/Assets/Player.cs
@@ -13,11 +13,24 @@ public class Player : MonoBehaviour
     private float acceleration = 13;
     private Vector2 velocity = Vector2.zero;
 
+    //screen wrapping
+    public Camera cam;
+    private Vector2 cam_bottom_left = Vector2.zero;
+    private Vector2 cam_top_right = Vector2.zero;
+    private float wrap_radius = 0.16f;
+
+    //Ships visuals
+    public GameObject ship_sprite;
+    private float jitter_amount = 0.04f;
+
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D> ();
 
+        cam_top_right = new Vector2(cam.scaledPixelWidth, cam.scaledPixelHeight);
+        cam_bottom_left = cam.ScreenToWorldPoint(cam_bottom_left);
+        cam_top_right = cam.ScreenToWorldPoint(cam_top_right);
     }
 
     // Update is called once per frame
@@ -33,14 +46,48 @@ public class Player : MonoBehaviour
             velocity += new Vector2(transform.up.x, transform.up.y) * acceleration * Time.fixedDeltaTime;
 
             velocity = Vector2.ClampMagnitude(velocity, max_speed);
+
+            //jitter
+            Vector2 jitter_pos = new Vector2(Random.Range(-jitter_amount, jitter_amount), Random.Range(-jitter_amount, jitter_amount));
+            ship_sprite.transform.localPosition = jitter_pos;
         }
 
         rb.MovePosition(rb.position + (velocity *Time.fixedDeltaTime));
 
+        #region Screen Wrap
+
+        if (rb.position.x - wrap_radius > cam_top_right.x)
+        {
+            rb.MovePosition(new Vector2(cam_bottom_left.x - wrap_radius + 0.01f, rb.position.y));
+        }
+
+        if (rb.position.x + wrap_radius < cam_bottom_left.x)
+        {
+            rb.MovePosition(new Vector2(cam_top_right.x + wrap_radius - 0.01f, rb.position.y));
+        }
+
+        if (rb.position.y - wrap_radius > cam_top_right.y)
+        {
+            rb.MovePosition(new Vector2(rb.position.x, cam_bottom_left.y - wrap_radius + 0.01f));
+        }
+
+        if (rb.position.y + wrap_radius < cam_bottom_left.y)
+        {
+            rb.MovePosition(new Vector2(rb.position.x, cam_top_right.y + wrap_radius - 0.01f));
+        }
+
+        #endregion
     }
 
     public void CaptureMoveInput(InputAction.CallbackContext context)
     {
         input_vec = context.ReadValue<Vector2>();
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, wrap_radius);
+
     }
 }
